7a701f56e2da643afa3acdff95b022ce0112e946
sannimdev <inegg.apps@gmail.com>
2021-11-07 16:49:00 +0900

    Node express 모듈 붙이기(with Typescript) #4

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,12 @@
-import { testMakeObject } from './utils/makeObject';
-
-testMakeObject();
+import express from 'express';
+class App {
+    public application: express.Application;
+    constructor() {
+        this.application = express();
+    }
+}
+const app = new App().application;
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('start');
+});
+app.listen(4000, () => console.log('start'));
