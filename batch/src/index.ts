cf6c1037303855c5c706fcddbe9cd74a3debbf33
sannimdev <inegg.apps@gmail.com>
2021-11-13 23:56:09 +0900

    MongoDB Atlas 붙이기 #11

--- batch/src/index.ts
+++ batch/src/index.ts
@@ -1,9 +1,13 @@
 import { testLand } from './land';
 import { saveFile } from './lib/file';
+import { addDocument } from './lib/mongo';
 
 async function run() {
     const houses = await testLand();
     await saveFile(`${Date.now()}-houses.json`, JSON.stringify(houses, null, 4));
+    const result = Array.isArray(houses) && (await addDocument('house', houses));
+    console.log(result);
+    process.exit();
 }
 
 run();
