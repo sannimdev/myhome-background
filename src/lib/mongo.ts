d8349bdc7186f4a27bc8f1bfbfb9d291361bea6c
sannimdev <inegg.apps@gmail.com>
2021-11-14 16:43:29 +0900

    Github actions 구동하기 #19

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -4,9 +4,12 @@ import dotenv from 'dotenv';
 
 // Connection URL
 const { parsed: env } = dotenv.config({ path: `${parse(__dirname).dir}/../.env` });
-const id = env ? env.MONGODB_ID : '';
-const pw = env ? env.MONGODB_PASSWORD : '';
-const dbName = env ? env.MONGODB_NAME : '';
+console.log('계정 =======================');
+const id = env ? env.MONGODB_ID : process.env.MONGODB_ID;
+const pw = env ? env.MONGODB_PW : process.env.MONGODB_PW;
+const dbName = env ? env.MONGODB_NAME : process.env.MONGODB_NAME;
+console.log(`id ${!!id}, pw ${!!pw}, dbName ${!!dbName}`);
+
 const url = `mongodb+srv://${id}:${pw}@cluster0.r7tht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
 
