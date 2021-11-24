1c080596ff0d86f28382f6f8e49a3b685794a528
sannimdev <inegg.apps@gmail.com>
2021-11-24 22:50:26 +0900

    로컬 머신 환경인지 Github Actions 환경인지 구별하는 모듈 작성 #26

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,25 @@ import { testComplex, testVilla } from './land';
 import { Room } from './type/land';
 // import { saveFile } from './lib/file';
 import { addDocument } from './lib/mongo';
+import { saveFile } from './lib/file';
+import { IS_LOCAL_MACHINE } from './lib/environment';
 
 async function run() {
     // await saveFile(`${Date.now()}-houses.json`, JSON.stringify(houses, null, 4));
     // const result = Array.isArray(houses) && (await addDocument('villa', houses));
     // console.log(result);
+    ///////////////////////////////////////////////////////////////////////////////////
     const houses = await Promise.allSettled([testComplex(), testVilla()]);
     const fulfilled = houses.filter(
         ({ status }) => status === 'fulfilled'
     ) as PromiseFulfilledResult<Room[]>[];
     const types = ['complex', 'villa'];
     const writeDB = await Promise.allSettled(
-        fulfilled.map(async ({ value }, idx) => addDocument(types[idx], value))
+        fulfilled.map(async ({ value }, idx) =>
+            IS_LOCAL_MACHINE
+                ? saveFile(`${Date.now()}-${types[idx]}.json`, JSON.stringify(value, null, 4))
+                : addDocument(types[idx], value)
+        )
     );
     console.log(writeDB);
     ////////////////////////////////// 프로세스 종료 명시적으로 해야 종료됨.
